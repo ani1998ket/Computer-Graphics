4309e730c0e84cc40b9c282910ea83d5edcbfd72
aniket <ani1998ket@gmail.com>
2020-07-17 19:36:01 +0530

    Added class for window management

---
+++ OpenGL/examples/Window.cpp
@@ -0,0 +1,50 @@
+#include <GL/glew.h>
+#include <iostream>
+#include "Window.hpp"
+
+Window::Window(int width, int height, const char* heading) 
+    : width(width), height(height), heading(heading)
+{
+    if( !glfwInit() ){
+
+    }
+    window = glfwCreateWindow(width, height,heading, NULL, NULL);
+    if(!window){
+
+    }
+    this->makeCurrent();
+    if( glewInit() != GLEW_OK ){
+        std::cerr << "GLEW: Initialisation Error.\n";
+    }
+
+    std::cout << "Window created\n";
+}
+Window::~Window() {
+    glfwDestroyWindow(window);
+    std::cout << "Window destroyed\n";
+}
+
+bool Window::isClosed() const {
+    return glfwWindowShouldClose(window);
+}
+
+GLFWwindow* Window::getContext() const {
+    return window;
+}
+
+void Window::makeCurrent() const {
+    glfwMakeContextCurrent(window);
+}
+
+void Window::swapBuffers() const{
+    glfwSwapBuffers( window );
+}
+
+void Window::pollEvents() const{
+    glfwPollEvents();
+}
+void Window::update() const{
+    glfwSwapInterval(1);
+    swapBuffers();
+    pollEvents();
+}
